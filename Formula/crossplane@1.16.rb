990a5b2c25fbdcfc53378fb5fef738a9d15b54f6
tampakrap <tampakrap@users.noreply.github.com>
2025-04-03 18:06:02 +0000

    feat(Formula/crossplane@1.16.rb): Bump to version `v1.16.5`

--- Formula/crossplane@1.16.rb
+++ Formula/crossplane@1.16.rb
@@ -1,28 +1,28 @@
 class CrossplaneAT116 < Formula
   desc 'The official Crossplane CLI'
   homepage 'https://crossplane.io'
-  version 'v1.16.3'
+  version 'v1.16.5'
   license 'Apache-2.0'
 
   if OS.mac? && Hardware::CPU.intel?
-    url 'https://releases.crossplane.io/stable/v1.16.3/bin/darwin_amd64/crank'
-    sha256 'e346641a1ba2bce2fc679a5565c13962c3725deee86c52462e4b909296720245'
+    url 'https://releases.crossplane.io/stable/v1.16.5/bin/darwin_amd64/crank'
+    sha256 '302f5f8c7ae90e70580ae16f1ddcc234c6c0c9becb8393a6bfde8813e98004e9'
   end
   if OS.mac? && Hardware::CPU.arm?
-    url 'https://releases.crossplane.io/stable/v1.16.3/bin/darwin_arm64/crank'
-    sha256 'ead0cf7c865c713432a3490bca4592663647d6d58251b1f3fa69daa33aabf7eb'
+    url 'https://releases.crossplane.io/stable/v1.16.5/bin/darwin_arm64/crank'
+    sha256 'b67565e7b366f396f71a45b36329dbbdcab2a4383400649ed461cca264886ddc'
   end
   if OS.linux? && Hardware::CPU.intel?
-    url 'https://releases.crossplane.io/stable/v1.16.3/bin/linux_amd64/crank'
-    sha256 '89b0e81fa95076fefe519e2788502a1c03b3064eedeebd35a8c62d38070ee9fd'
+    url 'https://releases.crossplane.io/stable/v1.16.5/bin/linux_amd64/crank'
+    sha256 '151f8b16ee6bd4093d19a438a556f65e13fe4f4ed54ec1a74e90e485c30ae05e'
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url 'https://releases.crossplane.io/stable/v1.16.3/bin/linux_arm/crank'
-    sha256 '8c58d4e520e5faf16e4b7cd959c0e7754805f5ab055d9721f86041188d003329'
+    url 'https://releases.crossplane.io/stable/v1.16.5/bin/linux_arm/crank'
+    sha256 '73e831aa19733c48ee1a57ecff5779cc0226c0c167d0c412198ad77069706de2'
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url 'https://releases.crossplane.io/stable/v1.16.3/bin/linux_arm64/crank'
-    sha256 'cd77f50df091ffba7eebd3271db43183ade2cd9d622793782d3662c0bc107836'
+    url 'https://releases.crossplane.io/stable/v1.16.5/bin/linux_arm64/crank'
+    sha256 'e7a2075bf2256d30afe8ac20b98b0e2fa64986375ba186e4d0886c03487423e4'
   end
 
   def install
