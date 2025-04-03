716b76a0b582ae3be095425e3bbb96b2d1efa93b
Theo Chatzimichos <tampakrap@gmail.com>
2025-04-03 20:20:54 +0200

    fix(Formula/crossplane@1.17.rb): fix checksums

--- Formula/crossplane@1.17.rb
+++ Formula/crossplane@1.17.rb
@@ -6,19 +6,19 @@ class CrossplaneAT117 < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url 'https://releases.crossplane.io/stable/v1.17.6/bin/darwin_amd64/crank'
-    sha256 '9e0c7d7f35bc4b0254f3d6960f3254a5f5680219c8ee5389362a6bb3291be757'
+    sha256 '18406c1364961c23627650ac4b848c0cb2b1164a7c9243f302bb54ae65fe6585'
   end
   if OS.mac? && Hardware::CPU.arm?
     url 'https://releases.crossplane.io/stable/v1.17.6/bin/darwin_arm64/crank'
-    sha256 '9e0c7d7f35bc4b0254f3d6960f3254a5f5680219c8ee5389362a6bb3291be757'
+    sha256 '78a3a61ddf300904d523e7b61bcb1a22498681e96abf0abfe856ce3c1550264f'
   end
   if OS.linux? && Hardware::CPU.intel?
     url 'https://releases.crossplane.io/stable/v1.17.6/bin/linux_amd64/crank'
-    sha256 '9e0c7d7f35bc4b0254f3d6960f3254a5f5680219c8ee5389362a6bb3291be757'
+    sha256 'cd927525c29666fbff9f722642919f4afb392a9e21da7dffc0244f330df91562'
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url 'https://releases.crossplane.io/stable/v1.17.6/bin/linux_arm/crank'
-    sha256 '9e0c7d7f35bc4b0254f3d6960f3254a5f5680219c8ee5389362a6bb3291be757'
+    sha256 'a98819492c586910962993cb031b8aea6941aaa52b202dffcca9a5b9fa96f246'
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url 'https://releases.crossplane.io/stable/v1.17.6/bin/linux_arm64/crank'
