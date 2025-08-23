bf4b68c056c82832466560dbf701a5a70ac5027f
tampakrap <tampakrap@users.noreply.github.com>
2025-08-23 15:55:10 +0000

    feat(Formula/crossplane.rb): Bump to version `1.20.1`

--- Formula/crossplane@2.0.rb
+++ Formula/crossplane@2.0.rb
@@ -1,28 +1,28 @@
-class Crossplane < Formula
+class CrossplaneAT20 < Formula
   desc "Build control planes without needing to write code"
   homepage "https://crossplane.io"
-  version "1.20.1"
+  version "2.0.2"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/darwin_amd64/crank.tar.gz"
-    sha256 "130bfcb44d40fb5da924c859ca1b1397a2a9be0e0a272a6c816b112be5d2ff44"
+    sha256 "4d64914fd0dfe893ad47b9064164bcd4ffc2d8e047e38515a79d1139bc8e762a"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/darwin_arm64/crank.tar.gz"
-    sha256 "db31604bc2d645307ec9c12cf2caa65e86e9a27fc57f74171c7265a3e5178f96"
+    sha256 "f1e8f68f6cb172cb67eaeec7d757aa40658533cb84f62752c9af67657fa3a449"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_amd64/crank.tar.gz"
-    sha256 "9987f2b42c4f1a5fbe26e2b1dc44ba2877589121e76161bbb1d80f38ad850b1f"
+    sha256 "33996b48004db9238c29b677d8166e92bfc9abef632e463ffefce5432b742d5e"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_arm/crank.tar.gz"
-    sha256 "7963937ffa90f94c9ac03306710d5b0fec1231d6ef3a0519e47457e460fce87f"
+    sha256 "37a6c10d80f5f5ea4318ac81a6f7f71c2ea0ad5d03c71db14f1e444308ab552f"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_arm64/crank.tar.gz"
-    sha256 "7e1eb7602e41c3d87061b5257b30154b795ea29ad6182b2ecdd7257e0b736452"
+    sha256 "4e5577c0dce69ee349e782151364d6608a2bae2b4ce5da127964ea7329f714ea"
   end
 
   def install
