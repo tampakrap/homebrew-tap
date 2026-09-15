813b292541aa93aa1a1bc68347f89b5e74e42138
Theo Chatzimichos <tampakrap@gmail.com>
2026-09-15 13:18:22 +0200

    fix checksums

--- Formula/crossplane.rb
+++ Formula/crossplane.rb
@@ -6,23 +6,23 @@ class Crossplane < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/darwin_amd64/crank.tar.gz"
-    sha256 "EMPTY1"
+    sha256 "919fa58a1c70a0139b58a60be696d1bfb4bf75908fa5638dec2e57300311d466"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/darwin_arm64/crank.tar.gz"
-    sha256 "EMPTY2"
+    sha256 "58b4e36a98685c72285cda267b6c0f5f8bf07d8381a8f8616fe2af20cc085519"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_amd64/crank.tar.gz"
-    sha256 "EMPTY3"
+    sha256 "98afad400cd355b9364a702c69d9c33d639606a660c82e5d01ae66742c5cf7b7"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_arm/crank.tar.gz"
-    sha256 "EMPTY4"
+    sha256 "51abf5da949fbdec8287f970e52f9ea9c3070dff7741f5ec44d44e8340f31e8e"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_arm64/crank.tar.gz"
-    sha256 "EMPTY5"
+    sha256 "d16e8c4d8af78751edb39e75b1c46b7164e1bdee0659a75e54ceb3262764a178"
   end
 
   def install
