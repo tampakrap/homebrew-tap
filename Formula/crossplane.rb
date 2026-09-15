f9422799fa8f29783398855e2efdc88347e1aeaa
tampakrap <tampakrap@users.noreply.github.com>
2026-09-15 11:44:54 +0000

    feat(Formula/crossplane.rb): Bump to version `2.3.4`

--- Formula/crossplane.rb
+++ Formula/crossplane.rb
@@ -1,32 +1,32 @@
 class Crossplane < Formula
   desc "Build control planes without needing to write code"
   homepage "https://crossplane.io"
-  version "2.2.5"
+  version "2.3.4"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://releases.crossplane.io/stable/v#{version}/bundle/darwin_amd64/crank.tar.gz"
-    sha256 "b5303641048cfd85264d5b80fad4a8e39b8c08e06906c61f3da25f7003133f46"
+    url "https://cli.crossplane.io/stable/v#{version}/bundle/darwin_amd64/crossplane-cli.tar.gz"
+    sha256 "919fa58a1c70a0139b58a60be696d1bfb4bf75908fa5638dec2e57300311d466"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://releases.crossplane.io/stable/v#{version}/bundle/darwin_arm64/crank.tar.gz"
-    sha256 "020628f8e650bc881667d07a08092d654b37736ca3f3e1bbf7449f71e933dea0"
+    url "https://cli.crossplane.io/stable/v#{version}/bundle/darwin_arm64/crossplane-cli.tar.gz"
+    sha256 "58b4e36a98685c72285cda267b6c0f5f8bf07d8381a8f8616fe2af20cc085519"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_amd64/crank.tar.gz"
-    sha256 "e2c3aec832ac4286e09b5eb057f6d73d306d2ff5d8ac14d50c62fdcd30a10ffc"
+    url "https://cli.crossplane.io/stable/v#{version}/bundle/linux_amd64/crossplane-cli.tar.gz"
+    sha256 "98afad400cd355b9364a702c69d9c33d639606a660c82e5d01ae66742c5cf7b7"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_arm/crank.tar.gz"
-    sha256 "5ad11a4782c70a7d1f1a22fe72f9240de4a0bc582705f1c2cf0213173d66fea8"
+    url "https://cli.crossplane.io/stable/v#{version}/bundle/linux_arm/crossplane-cli.tar.gz"
+    sha256 "51abf5da949fbdec8287f970e52f9ea9c3070dff7741f5ec44d44e8340f31e8e"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://releases.crossplane.io/stable/v#{version}/bundle/linux_arm64/crank.tar.gz"
-    sha256 "55af628d6dd785817d6d1a293f4767feb2805013c922b1b6df406688b2ac8f41"
+    url "https://cli.crossplane.io/stable/v#{version}/bundle/linux_arm64/crossplane-cli.tar.gz"
+    sha256 "d16e8c4d8af78751edb39e75b1c46b7164e1bdee0659a75e54ceb3262764a178"
   end
 
   def install
-    bin.install "crank" => "crossplane"
+    bin.install "crossplane"
   end
 
   test do
